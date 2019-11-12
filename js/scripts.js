3ec8fa7db19b198e68d6f46785ba50be6efdcb3f
shailesh <shaileshp163@gmail.com>
2019-11-12 18:06:38 +0530

    ok

--- js/scripts.js
+++ js/scripts.js
@@ -3,97 +3,101 @@ $(document).ready(function() {
 	/***************** Waypoints ******************/
 
 	$('.wp1').waypoint(function() {
-		$('.wp1').addClass('animated fadeInLeft');
+		$('.wp1').addClass('animated fadeInUp');
 	}, {
 		offset: '75%'
 	});
 	$('.wp2').waypoint(function() {
-		$('.wp2').addClass('animated fadeInDown');
+		$('.wp2').addClass('animated fadeInUp');
 	}, {
 		offset: '75%'
 	});
 	$('.wp3').waypoint(function() {
-		$('.wp3').addClass('animated bounceInDown');
-	}, {
-		offset: '75%'
-	});
-	$('.wp4').waypoint(function() {
-		$('.wp4').addClass('animated fadeInDown');
+		$('.wp3').addClass('animated fadeInRight');
 	}, {
 		offset: '75%'
 	});
 
-	/***************** Flickity ******************/
-
-	$('#featuresSlider').flickity({
-		cellAlign: 'left',
-		contain: true,
-		prevNextButtons: false
+	/***************** Initiate Flexslider ******************/
+	$('.flexslider').flexslider({
+		animation: "slide"
 	});
 
-	$('#showcaseSlider').flickity({
-		cellAlign: 'left',
-		contain: true,
-		prevNextButtons: false,
-		imagesLoaded: true
-	});
+	/***************** Initiate Fancybox ******************/
 
-	/***************** Fancybox ******************/
-
-	$(".youtube-media").on("click", function(e) {
-		var jWindow = $(window).width();
-		if (jWindow <= 768) {
-			return;
-		}
-		$.fancybox({
-			href: this.href,
-			padding: 4,
-			type: "iframe",
-			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-		});
-		return false;
-	});
-
-});
-
-$(document).ready(function() {
-	$("a.single_image").fancybox({
+	$('.single_image').fancybox({
 		padding: 4,
 	});
-});
 
-/***************** Nav Transformicon ******************/
+	/***************** Tooltips ******************/
+    $('[data-toggle="tooltip"]').tooltip();
 
-/* When user clicks the Icon */
-$(".nav-toggle").click(function() {
-	$(this).toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
-});
+	/***************** Nav Transformicon ******************/
 
-/* When user clicks a link */
-$(".overlay ul li a").click(function() {
-	$(".nav-toggle").toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
-});
+	/* When user clicks the Icon */
+	$('.nav-toggle').click(function() {
+		$(this).toggleClass('active');
+		$('.header-nav').toggleClass('open');
+		event.preventDefault();
+	});
+	/* When user clicks a link */
+	$('.header-nav li a').click(function() {
+		$('.nav-toggle').toggleClass('active');
+		$('.header-nav').toggleClass('open');
 
-/* When user clicks outside */
-$(".overlay").click(function() {
-	$(".nav-toggle").toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
-});
+	});
 
-/***************** Smooth Scrolling ******************/
+	/***************** Header BG Scroll ******************/
 
-$('a[href*=#]:not([href=#])').click(function() {
-	if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+	$(function() {
+		$(window).scroll(function() {
+			var scroll = $(window).scrollTop();
 
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		if (target.length) {
-			$('html,body').animate({
-				scrollTop: target.offset().top
-			}, 2000);
-			return false;
-		}
-	}
+			if (scroll >= 20) {
+				$('section.navigation').addClass('fixed');
+				$('header').css({
+					"border-bottom": "none",
+					"padding": "35px 0"
+				});
+				$('header .member-actions').css({
+					"top": "26px",
+				});
+				$('header .navicon').css({
+					"top": "34px",
+				});
+			} else {
+				$('section.navigation').removeClass('fixed');
+				$('header').css({
+					"border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+					"padding": "50px 0"
+				});
+				$('header .member-actions').css({
+					"top": "41px",
+				});
+				$('header .navicon').css({
+					"top": "48px",
+				});
+			}
+		});
+	});
+	/***************** Smooth Scrolling ******************/
+
+	$(function() {
+
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 2000);
+					return false;
+				}
+			}
+		});
+
+	});
+
 });
